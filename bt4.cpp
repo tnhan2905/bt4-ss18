7e27e812e8bed3e73a9a78d1cb41fa2eb950792b
tnhan2905 <diepthanhnhan2019.st@gmail.com>
2025-01-09 07:53:18 +0700

    Add files via upload

--- bt4.cpp
+++ bt4.cpp
@@ -1,14 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+#define size 50
 
-void inMang(int a[],int *n){
-	printf("Ca phan tu trong mang la: ");
-	for(int i=0;i<*n;i++){
-		printf("%d ",*(a+i));
-	}
-}
+struct sinhVien{
+	int id;
+	char name[100];
+	int age;
+	char phoneNum[100];
+};
+
+typedef struct sinhVien sinhVien;
 
 int main(){
-	int n=9;
-	int a[n]={1,2,3,4,5,6,7,8,9};
-	inMang(a,&n);
-} 
+	sinhVien arr[size];
+	for(int i=0;i<5;i++){
+		arr[i].id=i+1;
+		printf("Nhap vao ten sinh vien thu %d: ",i+1);
+		fgets(arr[i].name,100,stdin);
+		arr[i].name[strcspn(arr[i].name,"\n")]='\0';
+		
+		printf("Nhap vao tuoi sinh vien thu %d: ",i+1);
+		scanf("%d",&arr[i].age);
+		while(getchar() != '\n');
+		
+		printf("Nhap vao sdt sinh vien thu %d: ",i+1);
+		fgets(arr[i].phoneNum,100,stdin);
+		arr[i].phoneNum[strcspn(arr[i].phoneNum,"\n")]='\0';
+	}
+
+	printf("%-5s %-25s %-5s %-12s\n","ID","Name","Age","Phone Number");
+	for (int i = 0; i < 5; i++) {
+        printf("%-5d %-25s %-5d %-12s\n", arr[i].id, arr[i].name, arr[i].age, arr[i].phoneNum);
+    }
+}
